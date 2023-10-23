a6c61fbd6517a8a526cd6664a97ed106b71d4065
Csetox <csordasattila25@gmail.com>
2023-10-23 23:15:22 +0200

    Completed the DeleteTask() function.

--- TaskManagement/TaskManagement.cs
+++ TaskManagement/TaskManagement.cs
@@ -12,18 +12,29 @@ namespace TaskManagement
         {
             user.Tasks.Add(task);
         }
-        public void DeleteTask(Task task)
+        public void DeleteTask(User user, int taskID)
         {
+            Task? taskToRemove = user.Tasks.FirstOrDefault(task => task.taskID == taskID);
+            
+            if(taskToRemove is not null) user.Tasks.Remove(taskToRemove);
+        }
+        public void DeleteTask(User user, string taskName)
+        {
+            var taskToRemove = user.Tasks.Where(task => task.Title == taskName);
+
+            if(taskToRemove is not null)
+            {
+                if (taskToRemove.ToList().Count > 1) throw new MoreThanOneArgumentException("You have more than one matching tasks!");
+                else user.Tasks.Remove(taskToRemove.First());
+            }
+
 
         }
-        public void UpdateTask(Task task)
-        {
 
-        }
 
         public void ListTasks(User user)
         {
-            foreach(var item in user.Tasks) Console.WriteLine(item.Title);
+            foreach(var item in user.Tasks) Console.WriteLine(item.Title + " " + item.DueDate);
         }
 
 
@@ -31,11 +42,21 @@ namespace TaskManagement
         static void Main()
         {
             User Csetox = new User();
-            Csetox.Username = "Csetox";
+            User Foo = new User();
 
             TaskManagement Logic = new();
 
-            Logic.AddTask(Csetox, new Task("October 24, 2023 16:20:00", "foo", Priority.Medium, Status.Pending));
+            Logic.AddTask(Csetox, new Task("10/23/2024 16:20:00", "foo", Priority.Medium, Status.Pending));
+            Logic.AddTask(Csetox, new Task("10/23 16:20:00", "bar", Priority.Medium, Status.Pending));
+
+            Logic.AddTask(Foo, new Task("10/23/2024 16:20:00", "foo", Priority.Medium, Status.Pending));
+            Logic.AddTask(Foo, new Task("10/23/2024 16:20:00", "bar", Priority.Medium, Status.Pending));
+
+            Logic.ListTasks(Csetox);
+            Console.WriteLine();
+
+
+            Logic.DeleteTask(Csetox, "foo");
 
             Logic.ListTasks(Csetox);
 
