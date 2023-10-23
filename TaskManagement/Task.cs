ef4f1f82ab0ef0bc5134b9b3014ac8b646862250
Csetox <csordasattila25@gmail.com>
2023-10-23 22:49:14 +0200

    Made a DateTime converter, and added a taskID to the Task class to uniquely indentify each task.

--- TaskManagement/Task.cs
+++ TaskManagement/Task.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Collections.Specialized;
 
 namespace TaskManagement
 {
@@ -19,23 +20,46 @@ namespace TaskManagement
     }
     public class Task
     {
+        static int nextTaskID = 0;
+
         public DateTime DueDate;
         public string Title;
         public Priority PriorityLevel;
         public Status Status;
+        public int taskID { get; private set; }
 
         public Task(string dueDate, string title, Priority priorityLevel, Status status)
         {
             DueDate = ConvertStringToDateTime(dueDate);
             Title = title;
             PriorityLevel = priorityLevel;
-            Status = Status.Pending; ;
+            Status = Status.Pending;
+            taskID = nextTaskID;
+
+            nextTaskID++;
         }
         static DateTime ConvertStringToDateTime(string dateInString)
         {
-            string format = "MMMM dd, yyyy HH:mm:ss";
 
-            return DateTime.ParseExact(dateInString, format,CultureInfo.InvariantCulture);
+
+            string format1 = "MM/dd/yyyy HH:mm:ss";
+            string format2 = "MM/dd HH:mm:ss";
+
+            if(HasYearInDueDate(dateInString)) return DateTime.ParseExact(dateInString, format1,CultureInfo.InvariantCulture);
+            return DateTime.ParseExact(dateInString, format2, CultureInfo.InvariantCulture);
+            
+
+        }
+        static bool HasYearInDueDate(string dateInString)
+        {
+            int countOfSlashes = 0;
+
+            for (int i = 0; i < dateInString.Length; i++)
+            {
+                if (dateInString[i] == '/') countOfSlashes++;
+            }
+            
+            return countOfSlashes>1;
 
         }
 
