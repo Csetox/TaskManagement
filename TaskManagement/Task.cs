8e6f6a954cb079590e30bde56f84e337a88e9057
Csetox <csordasattila25@gmail.com>
2023-10-23 21:14:59 +0200

    Add project files.

---
+++ TaskManagement/Task.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace TaskManagement
+{
+    public enum Priority
+    {
+        Low,
+        Medium,
+        High
+    }
+    public enum Status
+    {
+        Pending,
+        Overdue,
+        Completed
+    }
+    class Task
+    {
+        public DateTime DueDate;
+        public string Title;
+        public Priority PriorityLevel;
+        public Status Status;
+
+        public Task(DateTime dueDate, string title, Priority priorityLevel, Status status)
+        {
+            DueDate = dueDate;
+            Title = title;
+            PriorityLevel = priorityLevel;
+            Status = status;
+        }
+
+        public void AddTask()
+        {
+
+        }
+
+        public void 
+
+    }
+
+
+}
