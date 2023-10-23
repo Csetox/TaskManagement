335e72384b14d929e62ff1046853595ed61d219e
Csetox <csordasattila25@gmail.com>
2023-10-23 23:14:45 +0200

    Made this Exception.cs for custom exceptions.

---
+++ TaskManagement/Exceptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagement
+{
+    public class MoreThanOneArgumentException : Exception
+    {
+        public MoreThanOneArgumentException(){}
+        public MoreThanOneArgumentException(string message) : base(message){}
+
+    }
+}
