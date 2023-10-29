8c23c53864b65000a30dd0bc79d5aeac93a191de
Csetox <csordasattila25@gmail.com>
2023-10-29 21:00:49 +0100

    Added a new Class to handle the logic of everything.

---
+++ TaskManagement/Application.cs
@@ -0,0 +1,24 @@
+﻿using Org.BouncyCastle.Tls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagement
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+
+            char registerOrNot = (char)Console.Read();
+
+            Console.WriteLine(registerOrNot);
+
+            //TaskManagement.Run();
+
+
+        }
+    }
+}
