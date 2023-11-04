80da4b8745de304d144a28ad2e31010dd32b9d41
Csetox <csordasattila25@gmail.com>
2023-11-04 02:00:41 +0100

    New class for exporting tasks in JSON.

---
+++ TaskManagement/Export.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TaskManagement
+{
+    public static class Export
+    {
+        public static void ExportToJson(User user)
+        {
+            List<Task> taskList = Database.GetTasksToList(user);
+
+            List<string> JsonStringList = new();
+
+            foreach(Task item in taskList)
+            {
+                string json = JsonSerializer.Serialize(item);
+
+                JsonStringList.Add(json);
+            }
+
+            foreach (var item in JsonStringList)
+            {
+                Console.WriteLine(item);
+            }
+
+            string downloadsPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Downloads\\tasks.json";
+            File.WriteAllLines(downloadsPath, JsonStringList);
+
+        }
+
+
+
+    }
+}
